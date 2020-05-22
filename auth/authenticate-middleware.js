9dc7a75ed7f8537d1e24093031199e7feeff610e
Julia Vu <juliadvu@gmail.com>
2020-05-22 10:22:03 -0500

    other dependencies installed and auth middleware made

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -3,6 +3,27 @@
   before granting access to the next middleware/route handler
 */
 
+const jwt = require("jsonwebtoken");
+
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  // add code here to verify users are logged in
+  const token = req.headers.authorization;
+
+  if (token) {
+    const secret = process.env.JWT_SECRET || "top secret!";
+
+    jwt.verify(token, secret, (error, decodedToken) => {
+      if (error) {
+        // the token is invalid
+        res.status(401).json({ message: "you cannot pass!" });
+      } else {
+        // the token is good
+        req.jwt = decodedToken;
+
+        next();
+      }
+    });
+  } else {
+    res.status(400).json({ message: "Please provide the authentication information" });
+  }
 };
